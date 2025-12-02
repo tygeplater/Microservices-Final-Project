75837bf2183ef42bcaa9b2c310f243085cd7b175
Tyge Plater <platert@msoe.edu>
2025-12-02 10:24:25 -0600

    Installing Frontend Libraries

--- f1-service/frontend/src/api/api.hub.ts
+++ f1-service/frontend/src/api/api.hub.ts
@@ -1,7 +1,7 @@
 
 // Fetch the Schedule for a Given Year
 export async function fetchSchedule(year: number): Promise<any> {
-    const response = await fetch(`/api/schedule/${year}`);
+    const response = await fetch(`/schedule/${year}`);
     if (!response.ok) {
         throw new Error(`Error fetching schedule for year ${year}: ${response.statusText}`);
     } else {
--- f1-service/frontend/src/models/models.ts
+++ f1-service/frontend/src/models/models.ts
@@ -1,4 +1,25 @@
-interface ScheduleResponse {
+export interface ScheduleResponse {
     status: number;
     schedule: any;
 };
+
+export interface ScheduleEvent {
+  RoundNumber: number;
+  Country: string;
+  Location: string;
+  OfficialEventName: string;
+  EventDate: string;
+  EventName: string;
+  EventFormat: string;
+  Session1: string;
+  Session1Date: string;
+  Session2: string;
+  Session2Date: string;
+  Session3: string;
+  Session3Date: string;
+  Session4: string;
+  Session4Date: string;
+  Session5: string;
+  Session5Date: string;
+  F1ApiSupport: boolean;
+}
